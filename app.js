3d6d297251e1e4da29b7ca0b4501496756bc3408
Stephen Keable <hello@stephenkeable.co.uk>
2016-08-16 19:57:29 +0100

    try this shit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 80));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/less-css', expressLess(__dirname + '/public/styles/', { compress: true }));
