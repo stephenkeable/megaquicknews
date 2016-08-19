f51a5505c0d083fbb4808c6cf5941f6545108654
Stephen Keable <hello@stephenkeable.co.uk>
2016-08-19 10:39:27 +0100

    news section

--- routes/news.js
+++ routes/news.js
@@ -38,7 +38,7 @@ router.get('/', function (req, res, next) {
     
 });
 
-// GET tv
+// GET uk
 
 router.get('/uk', function (req, res, next) {
             
@@ -53,6 +53,176 @@ router.get('/uk', function (req, res, next) {
             res.locals.news_items = guardian_object.response.results;
             
             res.locals.title = "UK News";
+
+        }
+
+        next();
+        
+    });
+}, function (req, res) {
+    
+    //set cache control headers
+	res.set("Cache-Control","max-age=1800");
+    res.set("Vary", "Accept-Encoding");
+
+    res.render('news' , { 
+  		hostname: req.hostname,
+  		website: req.website
+  	});
+    
+});
+
+// GET world
+
+router.get('/world', function (req, res, next) {
+            
+    var request_url = "https://content.guardianapis.com/world?api-key="+process.env.GUARDIAN_API_KEY+"&order-by=newest&tag=tone/news&show-fields=trailText";
+        
+    request(request_url, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            
+            guardian_object = JSON.parse(body);
+            
+            res.locals.news_items = guardian_object.response.results;
+            
+            res.locals.title = "World News";
+
+        }
+
+        next();
+        
+    });
+}, function (req, res) {
+    
+    //set cache control headers
+	res.set("Cache-Control","max-age=1800");
+    res.set("Vary", "Accept-Encoding");
+
+    res.render('news' , { 
+  		hostname: req.hostname,
+  		website: req.website
+  	});
+    
+});
+
+// GET tech
+
+router.get('/tech', function (req, res, next) {
+            
+    var request_url = "https://content.guardianapis.com/technology?api-key="+process.env.GUARDIAN_API_KEY+"&order-by=newest&tag=tone/news&show-fields=trailText";
+        
+    request(request_url, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            
+            guardian_object = JSON.parse(body);
+            
+            res.locals.news_items = guardian_object.response.results;
+            
+            res.locals.title = "Technology News";
+
+        }
+
+        next();
+        
+    });
+}, function (req, res) {
+    
+    //set cache control headers
+	res.set("Cache-Control","max-age=1800");
+    res.set("Vary", "Accept-Encoding");
+
+    res.render('news' , { 
+  		hostname: req.hostname,
+  		website: req.website
+  	});
+    
+});
+
+// GET science
+
+router.get('/science', function (req, res, next) {
+            
+    var request_url = "https://content.guardianapis.com/science?api-key="+process.env.GUARDIAN_API_KEY+"&order-by=newest&tag=tone/news&show-fields=trailText";
+        
+    request(request_url, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            
+            guardian_object = JSON.parse(body);
+            
+            res.locals.news_items = guardian_object.response.results;
+            
+            res.locals.title = "Science News";
+
+        }
+
+        next();
+        
+    });
+}, function (req, res) {
+    
+    //set cache control headers
+	res.set("Cache-Control","max-age=1800");
+    res.set("Vary", "Accept-Encoding");
+
+    res.render('news' , { 
+  		hostname: req.hostname,
+  		website: req.website
+  	});
+    
+});
+
+// GET business
+
+router.get('/business', function (req, res, next) {
+            
+    var request_url = "https://content.guardianapis.com/business?api-key="+process.env.GUARDIAN_API_KEY+"&order-by=newest&tag=tone/news&show-fields=trailText";
+        
+    request(request_url, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            
+            guardian_object = JSON.parse(body);
+            
+            res.locals.news_items = guardian_object.response.results;
+            
+            res.locals.title = "Business News";
+
+        }
+
+        next();
+        
+    });
+}, function (req, res) {
+    
+    //set cache control headers
+	res.set("Cache-Control","max-age=1800");
+    res.set("Vary", "Accept-Encoding");
+
+    res.render('news' , { 
+  		hostname: req.hostname,
+  		website: req.website
+  	});
+    
+});
+
+// GET money
+
+router.get('/money', function (req, res, next) {
+            
+    var request_url = "https://content.guardianapis.com/money?api-key="+process.env.GUARDIAN_API_KEY+"&order-by=newest&tag=tone/news&show-fields=trailText";
+        
+    request(request_url, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            
+            guardian_object = JSON.parse(body);
+            
+            res.locals.news_items = guardian_object.response.results;
+            
+            res.locals.title = "Money News";
 
         }
 
