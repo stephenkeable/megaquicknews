1e49e9033076ab2a579be7471d7c3535bceff2a1
Stephen Keable <hello@stephenkeable.co.uk>
2016-08-19 11:54:09 +0100

    about

--- routes/home.js
+++ routes/home.js
@@ -20,4 +20,23 @@ router.get('/', function (req, res, next) {
     
 });
 
+router.get('/about', function (req, res, next) {
+    
+    res.locals.page_title = "About 2G News";
+    
+    next();
+    
+}, function (req, res) {
+    
+    //set cache control headers
+	res.set("Cache-Control","max-age=86400");
+    res.set("Vary", "Accept-Encoding");
+
+    res.render('about' , { 
+  		title: res.locals.page_title,
+  		website: req.website
+  	});
+    
+});
+
 module.exports = router;
