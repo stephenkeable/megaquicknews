25314c6835589f2fca56d44a5fed3cf53bc073d8
Stephen Keable <hello@stephenkeable.co.uk>
2016-08-22 10:37:39 +0100

    Update sport.js

--- routes/sport.js
+++ routes/sport.js
@@ -8,7 +8,7 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
             
-    var request_url = "https://content.guardianapis.com/sport?api-key="+process.env.GUARDIAN_API_KEY+"&order-by=newest&tag=tone/matchreports,tone/news&show-fields=trailText";
+    var request_url = "https://content.guardianapis.com/sport?api-key="+process.env.GUARDIAN_API_KEY+"&order-by=newest&tag=-tone/minutebyminute&show-fields=trailText";
         
     request(request_url, function (error, response, body) {
 
