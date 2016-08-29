42b4c1096c46865dcacece43fcd3720bb0089f2a
Stephen Keable <hello@stephenkeable.co.uk>
2016-08-29 16:28:14 +0100

    tidy up

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
     
-    res.locals.page_title = "2G News";
+    res.locals.page_title = "2G News - News website designed to be fast even on a 2G phone.";
     
     next();
     
@@ -39,28 +39,28 @@ router.get('/about', function (req, res, next) {
     
 });
 
-router.get('/test', function (req, res, next) {
-    
-    res.locals.page_title = "Cloudfront test";
-    
-    next();
-    
-}, function (req, res) {
-    
-    //set cache control headers
-	res.set("Cache-Control","max-age=86400");
-    res.set("Vary", "Accept-Encoding");
-    
-    res.locals.is_mobile = req.get('CloudFront-Is-Mobile-Viewer');
-    res.locals.is_desktop = req.get('CloudFront-Is-Desktop-Viewer');
-    res.locals.is_tablet = req.get('CloudFront-Is-Tablet-Viewer');
-    res.locals.is_tv = req.get('CloudFront-Is-SmartTV-Viewer');
-
-    res.render('test' , { 
-  		title: res.locals.page_title,
-  		website: req.website
-  	});
-    
-});
+//router.get('/test', function (req, res, next) {
+//    
+//    res.locals.page_title = "Cloudfront test";
+//    
+//    next();
+//    
+//}, function (req, res) {
+//    
+//    //set cache control headers
+//	res.set("Cache-Control","max-age=86400");
+//    res.set("Vary", "Accept-Encoding");
+//    
+//    res.locals.is_mobile = req.get('CloudFront-Is-Mobile-Viewer');
+//    res.locals.is_desktop = req.get('CloudFront-Is-Desktop-Viewer');
+//    res.locals.is_tablet = req.get('CloudFront-Is-Tablet-Viewer');
+//    res.locals.is_tv = req.get('CloudFront-Is-SmartTV-Viewer');
+//
+//    res.render('test' , { 
+//  		title: res.locals.page_title,
+//  		website: req.website
+//  	});
+//    
+//});
 
 module.exports = router;
