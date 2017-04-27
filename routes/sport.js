1c3e3c2a4cf8bc67ccd90a574d2ed1bb7b316f66
Stephen Keable <hello@stephenkeable.co.uk>
2017-04-27 12:44:29 +0100

    rugby league typo

--- routes/sport.js
+++ routes/sport.js
@@ -67,7 +67,7 @@ router.get('/:section?', function (req, res, next) {
         page_title = 'Rugby Union News';
         guardian_tags = '-tone/minutebyminute,sport/rugby-union';
         
-    } else if (req.params.section == 'rugyby-league') {
+    } else if (req.params.section == 'rugby-league') {
         
         guardian_section = 'sport';
         page_title = 'Rugby League News';
